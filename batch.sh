56779ac6b30098d97a299e57e43d6de4994311b2
rdrew <robdrewupei@gmail.com>
2018-05-07 13:33:38 -0300

    most things working, need to process the output file

--- batch.sh
+++ batch.sh
@@ -23,6 +23,8 @@ echo -e "ocr exported as text to ./ocr/"
 
 grep -o -c -i -r -f wordlist.txt ocr/ >> searchresults.txt
 
-#sort this file according to keywords and remove duplicates?
-sort -t: +1 searchresults.txt >> searchresults_processed.txt
-
+#sort this file according to keywords
+# this sets ":" as field delimiter and chooses the second field(the keywords) to sort by 
+# the results are piped through uniq which strips redundant lines
+# this leaves unique filenames grouped by Keywords
+sort -t: +1 searchresults.txt | uniq -c >> searchresults_processed.txt
