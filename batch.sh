2f9a4bfaacb4763ada167f62b47b4bda29d545df
rdrew <robdrewupei@gmail.com>
2018-05-07 15:22:42 -0300

    rewind point

--- batch.sh
+++ batch.sh
@@ -15,16 +15,21 @@ for file in *.pdf
     do pdftotext "$file" "../ocr/$file.txt"
 done 
 
-echo -e "ocr exported as text to ./ocr/"
+echo "ocr exported as text to ./ocr/"
 
 ##############################################
 # search ocr output for keywords from wordlist
 ##############################################
 
-grep -o -c -i -r -f wordlist.txt ocr/ >> searchresults.txt
+grep -o -i -r -f wordlist.txt ocr/ > searchresults.txt
 
 #sort this file according to keywords
 # this sets ":" as field delimiter and chooses the second field(the keywords) to sort by 
 # the results are piped through uniq which strips redundant lines
 # this leaves unique filenames grouped by Keywords
-sort -t: +1 searchresults.txt | uniq -c >> searchresults_processed.txt
+
+sort -t: +1 searchresults.txt | uniq > searchresults_processed.txt
+
+#remove the dirname prefix leaving filenames grouped by keyword
+
+sed -e "s/ocr\/\///g" -i .backup searchresults_processed.txt
